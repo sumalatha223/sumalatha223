f0ca5a469e0410c84bd467e4c04ffa7fcdf0725b
sumalatha223 <163014871+sumalatha223@users.noreply.github.com>
2025-04-14 11:09:13 +0530

    Create exmaple data.py

---
+++ exmaple data.py
@@ -0,0 +1,7 @@
+players = [
+    {"name": "Virat Kohli", "role": "Batsman", "points_per_match": 100, "runs": 50, "wickets": 0, "catches": 1},
+    {"name": "Rohit Sharma", "role": "Batsman", "points_per_match": 90, "runs": 45, "wickets": 0, "catches": 0},
+    {"name": "Jasprit Bumrah", "role": "Bowler", "points_per_match": 80, "runs": 10, "wickets": 3, "catches": 1},
+    {"name": "MS Dhoni", "role": "Wicketkeeper", "points_per_match": 70, "runs": 30, "wickets": 0, "catches": 3},
+    {"name": "Kieron Pollard", "role": "Allrounder", "points_per_match": 85, "runs": 40, "wickets": 2, "catches": 2}
+]
